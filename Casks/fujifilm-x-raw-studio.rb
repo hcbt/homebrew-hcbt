9b627a5c67d12907d800d9be4b493948b1c0652a
hcbt <hcbt@protonmail.com>
2023-07-31 15:23:31 +0200

    Bump fujifilm-x-raw-studio to 1.19.0

--- Casks/fujifilm-x-raw-studio.rb
+++ Casks/fujifilm-x-raw-studio.rb
@@ -1,8 +1,8 @@
 cask "fujifilm-x-raw-studio" do
-  version "1.18.0"
-  sha256 "87cbdcbd9aa91396a23b5a2efbfab3244104a869e55aa82f5f17b746eecd1556"
+  version "1.19.0"
+  sha256 "39a85b9ca83190a4f4f0d9486bacc5d3da93dcb25b5a009164d47e715d682bf5"
 
-  url "https://dl.fujifilm-x.com/support/software/x-raw-studio-mac#{version.no_dots}-ihw1222x/XRawStudio#{version.no_dots}.dmg"
+  url "https://dl.fujifilm-x.com/support/software/x-raw-studio-mac#{version.no_dots}-5me31mwz/XRawStudio#{version.no_dots}.dmg"
   name "fujifilm-x-raw-studio"
   desc "FUJIFILM X RAW STUDIO is new RAW conversion software completely different from conventional RAW conversion software."
   homepage "https://fujifilm-x.com/support/download/software/x-raw-studio/"
