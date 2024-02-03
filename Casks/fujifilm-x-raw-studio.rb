31838be5d2bf64df02bea4ac17e6088e7cd58d6c
hcbt <git@hcbt.xyz>
2024-02-03 12:19:14 +0200

    Update x-raw-studio

--- Casks/fujifilm-x-raw-studio.rb
+++ Casks/fujifilm-x-raw-studio.rb
@@ -1,8 +1,8 @@
 cask "fujifilm-x-raw-studio" do
-  version "1.19.0"
-  sha256 "39a85b9ca83190a4f4f0d9486bacc5d3da93dcb25b5a009164d47e715d682bf5"
+  version "1.20.1"
+  sha256 "e772c4819c99d2fa4967cc50049bb97fb93f31ebc6b960955973ab359238f92a"
 
-  url "https://dl.fujifilm-x.com/support/software/x-raw-studio-mac#{version.no_dots}-5me31mwz/XRawStudio#{version.no_dots}.dmg"
+  url "https://dl.fujifilm-x.com/support/software/x-raw-studio-mac#{version.no_dots}-11f28kqi/XRawStudio#{version.no_dots}.dmg"
   name "fujifilm-x-raw-studio"
   desc "FUJIFILM X RAW STUDIO is new RAW conversion software completely different from conventional RAW conversion software."
   homepage "https://fujifilm-x.com/support/download/software/x-raw-studio/"
